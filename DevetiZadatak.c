75476d5d8448e9f2b0b312ee5897770486a2115a
Petar Ćulav <55332209+petarculav@users.noreply.github.com>
2022-01-11 15:46:15 +0100

    Update DevetiZadatak.c

--- DevetiZadatak.c
+++ DevetiZadatak.c
@@ -1,149 +1,114 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-struct tree;
-typedef struct tree* Node;
-typedef struct tree* Position;
-typedef struct tree {
+struct element;
+typedef struct element * position;
+typedef struct element
+{
 	int el;
-	Node right;
-	Node left;
-} Tree;
+	position left;
+	position right;
+}element;
 
-Node createEmptyTree(Node p);
-Node insertNode(Node p, int el);
-int printTree(Node p);
-Position findMin(Node p);
-Position findMax(Node p);
-Position find(Node p, int el);
-Node deleteNode(Node p, int el);
+position create_el(int a);
+position insert(position root, position p); //pod a)
+int replace(position root); //pod b)
+int printinorder(position root);
 
 int main()
 {
-	Tree tree;
-	Node root = &tree;
-
-	root = createEmptyTree(root);
-	root = insertNode(root, 5);
-	root = insertNode(root, 7);
-	root = insertNode(root, 4);
-	root = insertNode(root, 2);
-	root = insertNode(root, 9);
-	root = insertNode(root, 11);
-	root = insertNode(root, 1);
-	root = insertNode(root, 18);
-
-	printTree(root);
-
-	printf("\n");
-
-	root = deleteNode(root, 9);
-
-	printTree(root);
-
+	position korijen = NULL;
+	int i = 0;
+	int niz[10] = { 2, 5, 7, 8, 11, 1, 4, 2, 3, 7 };
+	for (i = 0; i < 10; i++)
+	{
+		korijen = insert(korijen, create_el(niz[i]));
+	}
+	printinorder(korijen);
+    puts("");
+	replace(korijen);
+	printinorder(korijen);
+    puts("");
 	return 0;
 }
 
-
-Node createEmptyTree(Node p) {
-	if (p != NULL)
-		return NULL;
-	else{
-		createEmptyTree(p->right);
-		createEmptyTree(p->left);
-		free(p);
+int printinorder(position q) {
+	if (q == NULL)
+	{
+		return EXIT_SUCCESS;
 	}
-	return NULL;
-}
-
-Node insertNode(Node p, int el) {
-	if (p == NULL) {
-		p = (Node)malloc(sizeof(Tree));
-		if (p == NULL) {
-			perror("Error allocating memory!\n");
-			return NULL;
-		}
-		p->el = el;
-		p->right = NULL;
-		p->left = NULL;
-	}
-
-	else if (p->el > el)
-		p->left = insertNode(p->left, el);
 	else
-		p->right = insertNode(p->right, el);
-	return p;
+	{
+		printinorder(q->left);
+		printf("%d\t", q->el);
+		printinorder(q->right);
+	}
+	return EXIT_SUCCESS;
 }
 
-int printTree(Node p) {
-	if (p != NULL) {
-		printTree(p->left);
-		printf("%d ", p->el);
-		printTree(p->right);
-	}
-
-	return 0;
-}
-
-Position findMin(Node p) {
-	if (p == NULL) {
-		printf("Element doesn't exist!\n");
-		return NULL;
-	}
+position create_el(int a)
+{
+	position q = (position)malloc(sizeof(element));
 	
-	else if (p->left == NULL)
-		return p;
-	else
-		return findMin(p->left);
+	q->el = a;
+
+	q->left = NULL;
+	q->right = NULL;
+
+	return q;
 }
 
-Position findMax(Node p) {
-	if (p != NULL) {
-		while(p->right != NULL)
-			p = p->right;
-		return p;
+position insert(position root, position p)
+{
+	if (root == NULL)
+	{
+		root = p;
+		return root;
 	}
-	else
-		return p;
+	else if (root->el <= p->el)
+	{
+		root->left = insert(root->left, p);
+	}
+	else if (root->el > p->el)
+	{
+		root->right = insert(root->right, p);
+	}
+	return root;
 }
 
-Position find(Node p, int el) {
-	if (p == NULL) {
-		printf("Element doesn't exist!\n");
-		return NULL;
+int replace(position current)
+{
+	int l_before = 0;
+	int d_before = 0;
+	int l_now = 0;
+	int d_now = 0;
+
+	if (current != NULL)
+	{
+		if (current->left != NULL)
+		{
+			l_before = current->left->el;
+		}
+		if (current->right != NULL)
+		{
+			d_before = current->right->el;
+		}
+
+		replace(current->left);
+		replace(current->right);
+
+		if (current->left != NULL)
+		{
+			l_now = current->left->el;
+		}
+
+		if (current->right != NULL)
+		{
+			d_now = current->right->el;
+		}
+
+		return current->el = l_now + d_now + l_before + d_before;
 	}
-
-	else if (p->el > el)
-		return find(p->left, el);
-	else if (p->el < el)
-		return find(p->right, el);
-	else
-		return p;
-}
-
-Node deleteNode(Node p, int el) {
-	Position temp = NULL;
-
-	if (p == NULL)
-		return NULL;
-	else if (p->el > el)
-		p->left = deleteNode(p->left, el);
-	else if (p->el < el)
-		p->right = deleteNode(p->right, el);
-	else if (p->right && p->left != NULL) {
-		temp = findMin(p->right);
-		p->el = temp->el;
-		p->right = deleteNode(p->right, p->el);
-	}
-	else {
-		temp = p;
-		if (p->left == NULL)
-			p = p->right;
-		else
-			p = p->left;
-		free(temp);
-	}
-
-	return p;
+	return 0;
 }
